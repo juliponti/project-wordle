4afa01a7287d72c5321ee738324d0bb7e5969c30
Julieta Pontino <julieta.pontino@gmail.com>
2024-12-17 20:47:20 -0300

    add missing letter

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@ export const NUM_OF_GUESSES_ALLOWED = 6;
 
 export const KEWBOARD_LETTERS = {
   firstRow: ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  secondRow: ["A", "S", "D", "F", "G", "J", "K", "L"],
+  secondRow: ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
   thirdRow: ["Z", "X", "C", "V", "B", "N", "M"],
 };
