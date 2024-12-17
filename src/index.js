70738edca92854098ea2ab1ada087a336e377adc
Julieta Pontino <julieta.pontino@gmail.com>
2024-12-17 16:12:57 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import App from "./components/App";
 
-import App from './components/App';
+import "./reset.css";
+import "./styles.css";
 
-import './reset.css';
-import './styles.css';
-
-const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
+const root = createRoot(document.querySelector("#root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/project-wordle" />} />
+      <Route path="/project-wordle" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
